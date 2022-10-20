9528b71dd74800f383ab253a402bf2f5819f1169
bths-ax <ardenx@nycstudents.net>
2022-10-20 08:25:09 -0400

    finished everything (again) (again)

---
+++ src/Example2.java
@@ -0,0 +1,13 @@
+public class Example2
+{
+	public static void main(String[] args)
+	{
+		Integer i = 2;
+		Double d = 5.0;
+		System.out.println( i.intValue() );
+		System.out.println( d.doubleValue() );
+		// Print out the min and max values possible for integers
+		System.out.println( Integer.MIN_VALUE );
+		System.out.println( Integer.MAX_VALUE );
+	}
+}
---
+++ src/Example3.java
@@ -0,0 +1,8 @@
+public class Example3 {
+	public static void main(String[] args) {
+		System.out.println(Integer.signum(-1));
+		System.out.println(Integer.signum(-5));
+		System.out.println(Integer.signum(1));
+		System.out.println(Integer.signum(5));
+	}
+}
